e6eb81fd869c1ef4787b53f460b06cffb58b6b1c
kanogame <wasdqert1222@gmail.com>
2024-01-24 18:43:22 +0700

    adding stats

--- stats/stats.go
+++ stats/stats.go
@@ -5,10 +5,11 @@ import (
 	"main/utils"
 )
 
-func Get_stats(folderData []utils.File) {
+func Get_stats(folderData []utils.File, folderSize int64) {
 	var lang_size_stat = make(map[string]utils.Language)
-
+	var sum int64 = 0
 	for _, file := range folderData {
+		sum += file.File_lenght
 		data, exist := lang_size_stat[file.Extension]
 		if exist {
 			lang_size_stat[file.Extension] = utils.Language{Lenght: data.Lenght + file.File_lenght, FileCount: data.FileCount + 1}
@@ -17,5 +18,9 @@ func Get_stats(folderData []utils.File) {
 		lang_size_stat[file.Extension] = utils.Language{Lenght: file.File_lenght, FileCount: 1}
 	}
 
-	fmt.Println(lang_size_stat)
+	fmt.Printf("%vk characters\n", sum/1000)
+	fmt.Println(folderSize)
+	for lang, data := range lang_size_stat {
+		fmt.Printf(".%v: %v%%\n", lang, (100*data.Lenght)/sum)
+	}
 }
